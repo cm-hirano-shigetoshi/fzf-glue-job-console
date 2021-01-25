d6919cb90cef5efe1254c3598002e62cb514ab90
hirano.shigetoshi <hirano.shigetoshi@classmethod.jp>
2021-01-25 23:51:13 +0900

    require MFA

--- glue.sh
+++ glue.sh
@@ -4,6 +4,7 @@ set -eu
 TOOL_DIR=$(dirname $(python3 -c "import os; print(os.path.realpath('$0'));"))
 
 if [[ $# > 0 ]]; then
+    aws sts get-caller-identity >/dev/null 2>&1
     fzfyml3 run ${TOOL_DIR}/runs.yml $1
 else
     echo "[Usage] bash $0 <job>-name" >&2
